205d975fee1c527e78653c12cc6b297727a11c10
Surya Rani <srani@ucsc.edu>
2021-08-04 17:05:02 -0700

    adding main python flask program

---
+++ website.py
@@ -0,0 +1,30 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def index():
+    
+    instagram = url_for('static', filename='instagram.png')
+    email = url_for('static', filename='email.png')
+    linkedin = url_for('static', filename='linkedin.png')
+    github = url_for('static', filename='github.png')
+
+
+    return render_template("index.html",instagram=instagram,email=email,linkedin=linkedin,github=github)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
